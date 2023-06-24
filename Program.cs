84ba0f9c5138ef47dc3d2de76d3d3485dbf371bc
Julian Simpson <simpsonjulian@gmail.com>
2023-06-24 16:59:08 +1200

    Kicking it old school

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Test
+{
+    public static string ComputeMd5Hash(string message)
+    {
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] input = Encoding.ASCII.GetBytes(message);
+            byte[] hash = md5.ComputeHash(input);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+    public static void Main()
+    {
+        string message = "Old School Dot Nets are not Bot Nets";
+        Console.WriteLine(ComputeMd5Hash(message));
+    }
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+var MD5 = require("crypto-js/md5");
+console.log(MD5("Hash this with corned beef and a shiraz").toString());
