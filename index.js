762a2abc4cf40b9813dffed0c7549582a9cfcef0
Julian Simpson <simpsonjulian@gmail.com>
2023-06-25 12:00:25 +1200

    Add script password hashing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+var key = new Buffer("");
+var scrypt = require("scrypt");
 var MD5 = require("crypto-js/md5");
+
 console.log(MD5("Hash this with corned beef and a shiraz").toString());
+ 
+var result = scrypt.hashSync(key,{"N":16,"r":1,"p":1},64,"");
+console.log(result.toString("hex"));
